07c57a79aa58990842c343f9882ec8eb40e4af44
dev-raimundos <devraimundos@outlook.com>
2026-02-09 20:14:45 -0300

    Feat: implementa zoneless.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
@@ -6,7 +6,7 @@ import { httpInterceptor } from './core/interceptors/http.interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
   ],
