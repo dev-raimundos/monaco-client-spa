61e696ba42951bc1a6288ac82088eff419ea4358
dev-raimundos <devraimundos@outlook.com>
2026-02-09 22:01:26 -0300

    Feat: Implementa sistema de rotas por domínio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations'; // Troquei para evitar o aviso
+
 import { routes } from './app.routes';
-import { httpInterceptor } from './core/interceptors/http.interceptors';
+import { httpInterceptor } from '@core/interceptors/http.interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
+    provideAnimations(),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./app').then((m) => m.App),
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+  },
+];
