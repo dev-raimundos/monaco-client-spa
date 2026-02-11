8016a3028fe1db0c04a9c21071db5f90a503505d
dev-raimundos <devraimundos@outlook.com>
2026-02-11 09:39:33 -0300

    Refactor: remove libs do Spartan UI

--- src/app/shared/spartan/combobox/src/lib/hlm-combobox-value.ts
+++
@@ -1,5 +0,0 @@
-import { Directive } from '@angular/core';
-import { BrnComboboxValue } from '@spartan-ng/brain/combobox';
-
-@Directive({ selector: '[hlmComboboxValue]', hostDirectives: [BrnComboboxValue] })
-export class HlmComboboxValue {}
